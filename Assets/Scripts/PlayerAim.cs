67ba33a1cf1c83c12bce60c51d145d0faf7cb659
Allison Girton <AllisonCGrimm@gmail.com>
2021-02-21 21:33:02 -0600

    Initial Commit
    Everything so far

---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");//finds the aim game object attached to the player
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = GetWorldMousePosition();//Gets current position of the mouse
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;//Gets the correct angle for the object to look at
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);//Makes the object point at the mouse
+    }
+
+    public static Vector3 GetWorldMousePosition()
+    {
+        Vector3 vec = GetWorldMousePositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetWorldMousePositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    public Text wealthText;
+    public int currentWealth = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateWealth(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            UpdateWealth(1);
+        }
+    }
+
+    public void UpdateWealth(int update)
+    {
+        currentWealth += update;
+        wealthText.text = currentWealth.ToString();
+    }
+}
